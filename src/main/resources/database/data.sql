8a18cd719babadd69b41b2292d95076d573617ef
Bananahanack <jurichus94@gmail.com>
2021-06-09 19:26:45 +0200

    H2 DB dependency added in pom.xml; Filled data.sql; Created application.properties file Some fixes in model Employee.java

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (last_name, first_name, middle_name, position) VALUES ('harley', 'alex', 'endurovich', 'angular-master');
+INSERT INTO employee (last_name, first_name, middle_name, position) VALUES ('vadim', 'vadik', 'vadimovich', 'vue-master');
+INSERT INTO employee (last_name, first_name, middle_name, position) VALUES ('ivanov', 'ivan', 'belarusovich', 'react-master');
+INSERT INTO employee (last_name, first_name, middle_name, position) VALUES ('apagasyan', 'alex', 'ismailovich', 'insert-data-master');
+INSERT INTO employee (last_name, first_name, middle_name, position) VALUES ('strawberry', 'berry', 'straw', 'berry-master');
+INSERT INTO employee (last_name, first_name, position) VALUES ('noname', 'nonamovich', 'pyton-master');
